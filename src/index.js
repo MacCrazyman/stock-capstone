f2a1ed44ccfdb9a3586e9a4baf51c4fae39e22fc
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-01 11:34:08 -0500

    Fix Gill font

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './app/store';
 import Indexes from './components/indexes';
 import Details from './components/details';
 import NoPage from './components/noMatch';
-import './fonts/gill_sans/Gill Sans.otf';
+import './fonts/gillsans/gillsans.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
