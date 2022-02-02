59faefdb16d508e400f66b8c9b37c727e0399357
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-02 11:17:53 -0500

    Fix render when directly loading a details page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Outlet } from 'react-router-dom';
 import './App.css';
 import getCompanies from './app/fetch';
@@ -7,12 +7,11 @@ import { loadCompanies } from './redux/reducer';
 
 function App() {
   const dispatch = useDispatch();
-
+  const companiesStore = useSelector((store) => store.companies);
   useEffect(async () => {
     const companies = await getCompanies();
     dispatch(loadCompanies(companies));
   }, []);
-
   return (
     <div className="App">
       <header className="App-header mx-auto bg-blue flex px-4">
@@ -20,7 +19,7 @@ function App() {
         <h2 className="mx-auto"> Companies </h2>
       </header>
       <main>
-        <Outlet />
+        {companiesStore.length !== 0 ? <Outlet /> : 'Loading Data...'}
       </main>
       <footer>
         CC License
--- src/components/indexes.js
+++ src/components/indexes.js
@@ -10,7 +10,7 @@ const Indexes = () => {
       <div className="w-40 mx-auto p-4"><img className="w-full" src={nasdaqLogo} alt="Nasdaq Logo" /></div>
       <h2 className="mx-auto object-center">Nasdaq companies!</h2>
       <h3 className="col-span-full text-left bg-blue-dark">List of companies</h3>
-      {companies.slice(0, 12).map((company, index) => (
+      {companies.slice(0, 98).map((company, index) => (
         <Link to={company.symbol} key={company.name} className={(index + 2) % 4 === 0 || (index + 3) % 4 === 0 ? 'bg-odd-blue' : 'bg-even-blue'}>
           <h4>{company.name}</h4>
           <p>
