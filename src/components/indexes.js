1ab5d2eacecfc5169037115855329d69a4219649
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-03 15:28:01 -0500

    Add fonts and style companies links

--- src/components/indexes.js
+++ src/components/indexes.js
@@ -8,19 +8,28 @@ const Indexes = () => {
     <div className="grid grid-cols-2 bg-blue2">
 
       <div className="w-40 mx-auto p-4"><img className="w-full" src={nasdaqLogo} alt="Nasdaq Logo" /></div>
-      <h2 className="mx-auto object-center">Nasdaq companies!</h2>
-      <h3 className="col-span-full text-left bg-blue-dark">List of companies</h3>
+      <div className="m-auto">
+        <h2 className="m-auto uppercase font-extrabold">Nasdaq companies!</h2>
+        <p className="m-auto capitalize">
+          {companies.length}
+          {' companies'}
+        </p>
+      </div>
+      <h3 className="col-span-full text-left bg-blue-dark px-2 uppercase">List of companies</h3>
       {companies.slice(0, 98).map((company, index) => (
-        <Link to={company.symbol} key={company.name} className={(index + 2) % 4 === 0 || (index + 3) % 4 === 0 ? 'bg-odd-blue' : 'bg-even-blue'}>
-          <h4>{company.name}</h4>
-          <p>
-            Location:
-            {company.headQuarter}
-          </p>
-          <p>
-            Symbol:
-            {company.symbol}
-          </p>
+        <Link to={company.symbol} key={company.name} className={`${(index + 2) % 4 === 0 || (index + 3) % 4 === 0 ? 'bg-odd-blue' : 'bg-even-blue'} grid grid-rows-3`}>
+          <div className="m-auto row-span-2">
+            <p className="font-extrabold text-7xl text-blue2">
+              {company.symbol}
+            </p>
+          </div>
+          <div className="p-4 text-right">
+            <h4 className="font-extrabold uppercase">{company.name}</h4>
+            <p>
+              {company.headQuarter}
+            </p>
+          </div>
+
         </Link>
       ))}
     </div>
