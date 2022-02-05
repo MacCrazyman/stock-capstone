b94d1b61babae2654cb9b598e2cc86847191cba7
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-04 19:39:52 -0500

    Fix linters

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -8,42 +8,42 @@ import App from './App';
 
 const companies = [
   {
-      "symbol": "ATVI",
-      "name": "Activision Blizzard",
-      "sector": "Communication Services",
-      "subSector": "Communication Services",
-      "headQuarter": "Santa Monica, CALIFORNIA",
-      "dateFirstAdded": null,
-      "cik": "0000718877",
-      "founded": "1983-06-10"
+    symbol: 'ATVI',
+    name: 'Activision Blizzard',
+    sector: 'Communication Services',
+    subSector: 'Communication Services',
+    headQuarter: 'Santa Monica, CALIFORNIA',
+    dateFirstAdded: null,
+    cik: '0000718877',
+    founded: '1983-06-10',
   },
   {
-      "symbol": "ADBE",
-      "name": "Adobe",
-      "sector": "Technology",
-      "subSector": "Technology",
-      "headQuarter": "San Jose, CALIFORNIA",
-      "dateFirstAdded": null,
-      "cik": "0000796343",
-      "founded": "1986-01-08"
+    symbol: 'ADBE',
+    name: 'Adobe',
+    sector: 'Technology',
+    subSector: 'Technology',
+    headQuarter: 'San Jose, CALIFORNIA',
+    dateFirstAdded: null,
+    cik: '0000796343',
+    founded: '1986-01-08',
   },
   {
-      "symbol": "ADP",
-      "name": "ADP",
-      "sector": "Industrials",
-      "subSector": "Industrials",
-      "headQuarter": "Roseland, NEW JERSEY",
-      "dateFirstAdded": null,
-      "cik": "0000008670",
-      "founded": "1961-09-01"
+    symbol: 'ADP',
+    name: 'ADP',
+    sector: 'Industrials',
+    subSector: 'Industrials',
+    headQuarter: 'Roseland, NEW JERSEY',
+    dateFirstAdded: null,
+    cik: '0000008670',
+    founded: '1961-09-01',
   },
-]
+];
 
-describe('Testing APP component',()=> {
-  test('Rendering companies',() => {
+describe('Testing APP component', () => {
+  test('Rendering companies', () => {
     const middlewares = [thunk, logger];
     const mockStore = configureStore(middlewares);
-    const store = mockStore({ companies: companies });
+    const store = mockStore({ companies });
     const tree = render(
       <Provider store={store}>
         <BrowserRouter>
@@ -52,5 +52,5 @@ describe('Testing APP component',()=> {
       </Provider>,
     );
     expect(tree).toMatchSnapshot();
-  })
-})
+  });
+});
--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -1,73 +1,69 @@
-import stockReducer from "./reducer"
+import stockReducer from './reducer';
 
 describe('Tests for reducers', () => {
-  const state = []
+  const state = [];
   const companies = [
     {
-        "symbol": "ATVI",
-        "name": "Activision Blizzard",
-        "sector": "Communication Services",
-        "subSector": "Communication Services",
-        "headQuarter": "Santa Monica, CALIFORNIA",
-        "dateFirstAdded": null,
-        "cik": "0000718877",
-        "founded": "1983-06-10"
+      symbol: 'ATVI',
+      name: 'Activision Blizzard',
+      sector: 'Communication Services',
+      subSector: 'Communication Services',
+      headQuarter: 'Santa Monica, CALIFORNIA',
+      dateFirstAdded: null,
+      cik: '0000718877',
+      founded: '1983-06-10',
     },
     {
-        "symbol": "ADBE",
-        "name": "Adobe",
-        "sector": "Technology",
-        "subSector": "Technology",
-        "headQuarter": "San Jose, CALIFORNIA",
-        "dateFirstAdded": null,
-        "cik": "0000796343",
-        "founded": "1986-01-08"
+      symbol: 'ADBE',
+      name: 'Adobe',
+      sector: 'Technology',
+      subSector: 'Technology',
+      headQuarter: 'San Jose, CALIFORNIA',
+      dateFirstAdded: null,
+      cik: '0000796343',
+      founded: '1986-01-08',
     },
     {
-        "symbol": "ADP",
-        "name": "ADP",
-        "sector": "Industrials",
-        "subSector": "Industrials",
-        "headQuarter": "Roseland, NEW JERSEY",
-        "dateFirstAdded": null,
-        "cik": "0000008670",
-        "founded": "1961-09-01"
+      symbol: 'ADP',
+      name: 'ADP',
+      sector: 'Industrials',
+      subSector: 'Industrials',
+      headQuarter: 'Roseland, NEW JERSEY',
+      dateFirstAdded: null,
+      cik: '0000008670',
+      founded: '1961-09-01',
     },
-  ]
+  ];
   const details = {
-    "symbol": "ADBE",
-    "name": "Adobe Inc.",
-    "price": 513.54,
-    "changesPercentage": 0.5305075,
-    "change": 2.7099915,
-    "dayLow": 503.77,
-    "dayHigh": 519.97,
-    "yearHigh": 699.54,
-    "yearLow": 420.78,
-    "marketCap": 242236801024,
-    "priceAvg50": 572.0876,
-    "priceAvg200": 588.24,
-    "volume": 2536737,
-    "avgVolume": 3403438,
-    "exchange": "NASDAQ",
-    "open": 503.77,
-    "previousClose": 510.83,
-    "eps": 10.02,
-    "pe": 51.25149,
-    "earningsAnnouncement": "2022-03-22T20:00:00.000+0000",
-    "sharesOutstanding": 471699967,
-    "timestamp": 1644020771
-}
-  const updatedCompany = {...companies[1],details}
+    symbol: 'ADBE',
+    name: 'Adobe Inc.',
+    price: 513.54,
+    changesPercentage: 0.5305075,
+    change: 2.7099915,
+    dayLow: 503.77,
+    dayHigh: 519.97,
+    yearHigh: 699.54,
+    yearLow: 420.78,
+    marketCap: 242236801024,
+    priceAvg50: 572.0876,
+    priceAvg200: 588.24,
+    volume: 2536737,
+    avgVolume: 3403438,
+    exchange: 'NASDAQ',
+    open: 503.77,
+    previousClose: 510.83,
+    eps: 10.02,
+    pe: 51.25149,
+    earningsAnnouncement: '2022-03-22T20:00:00.000+0000',
+    sharesOutstanding: 471699967,
+    timestamp: 1644020771,
+  };
+  const updatedCompany = { ...companies[1], details };
 
   test('Add companies to store', () => {
-    expect(stockReducer(state, {type: 'app/reducer/LOAD_COMPANIES', payload:companies})).toEqual(companies)
-  })
+    expect(stockReducer(state, { type: 'app/reducer/LOAD_COMPANIES', payload: companies })).toEqual(companies);
+  });
   test('Add details to a company', () => {
-    expect(stockReducer(companies, {type: 'app/reducer/LOAD_DETAILS', payload: details})).toEqual([companies[0],updatedCompany,companies[2]])
-  })
-
-
-})
-
-
+    expect(stockReducer(companies, { type: 'app/reducer/LOAD_DETAILS', payload: details })).toEqual([companies[0], updatedCompany, companies[2]]);
+  });
+});
