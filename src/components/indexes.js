5851f66fc92c3cc116043e8a2102a68c44396c4d
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-05 07:03:14 -0500

    Fix overflow problem

--- src/components/indexes.js
+++ src/components/indexes.js
@@ -16,15 +16,15 @@ const Indexes = () => {
 
         <div className="w-40 mx-auto p-4"><img className="w-full" src={nasdaqLogo} alt="Nasdaq Logo" /></div>
         <div className="my-auto p-4 text-left">
-          <h2 className="m-auto uppercase font-extrabold ">Nasdaq companies!</h2>
-          <p className="m-auto capitalize">
+          <h2 className="m-auto uppercase font-extrabold break-words">Nasdaq companies!</h2>
+          <p className="m-auto capitalize break-words">
             {companies.length}
             {' companies'}
           </p>
         </div>
         <div className="col-span-full text-left bg-blue-dark px-2 flex py-1 gap-4">
-          <h3 className="uppercase">List of companies</h3>
-          <input onChange={(e) => handleChange(e)} id="nameFilter" type="text" className="rounded-lg px-4 text-back" placeholder="Search company" />
+          <h3 className="uppercase break-words">List of companies</h3>
+          <input onChange={(e) => handleChange(e)} id="nameFilter" type="text" className="rounded-lg px-4 text-back break-words" placeholder="Search company" />
         </div>
       </div>
       {companies.filter((company) => RegExp(nameFilter)
@@ -33,13 +33,13 @@ const Indexes = () => {
           <Link to={company.symbol} key={company.name} className={`${(index + 2) % 4 === 0 || (index + 3) % 4 === 0 ? 'bg-odd-blue' : 'bg-even-blue'} grid grid-rows-3 relative`}>
             <div className="absolute top-0 right-0 p-2">&#10162;</div>
             <div className="m-auto row-span-2">
-              <p className="font-extrabold text-5xl text-blue2 ">
+              <p className="font-extrabold text-5xl text-blue2 break-words">
                 {company.symbol}
               </p>
             </div>
             <div className="p-4 text-right">
-              <h4 className="font-extrabold uppercase ">{company.name}</h4>
-              <p className="">
+              <h4 className="font-extrabold uppercase break-words">{company.name}</h4>
+              <p className="break-words">
                 {company.headQuarter}
               </p>
             </div>
